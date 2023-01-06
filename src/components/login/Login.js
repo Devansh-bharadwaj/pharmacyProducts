a5588e22c90d327f3be27c19ea16745c9f107e3a
Devansh <91583975+Devansh-bharadwaj@users.noreply.github.com>
2023-01-06 17:27:33 +0530

    updated

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
-import styles from './Login.module.css';
+import React, { useState } from "react";
+import styles from "./Login.module.css";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Login(props) {
-    const [username, setUsername] = useState("");
+  const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
@@ -17,36 +19,49 @@ function Login(props) {
     setPassword(value);
   }
 
-
   function loginHandler(event) {
     event.preventDefault();
 
-    if(username === "qaifi" && password === "qaifi"){
-      props.onLogin(true)
-      alert('You have successfully logged in 😀')
-      navigate('/')
-    }else{
-        alert('Please enter correct password 🙃');
+    if (username === "qaifi" && password === "qaifi") {
+      props.onLogin(true);
+      alert("You have successfully logged in 😀");
+      navigate("/");
+    } else {
+      alert("Please enter username and password as 'qaifi' 🙃");
     }
-
   }
+  
 
   return (
-    <div className='container' style={{marginTop: "115px"}}>
+    <>
+      <div className="container" style={{ marginTop: "115px" }}>
         <div>
           <div className={styles.formMainDiv}>
             <form className={styles.formDiv} onSubmit={loginHandler}>
-                <h1 className={styles.formHeading}>Sign In</h1>
-                <input type="text" onChange={usernameChangeHandler}
-                required placeholder="Enter Username" className={styles.userDiv} />
-                <input type="password" onChange={passwordChangeHandler}
-                required placeholder="Enter Password" className={styles.userDiv} />
-                <button type="submit" className={styles.loginDiv}>Login</button>
+              <h1 className={styles.formHeading}>Sign In</h1>
+              <input
+                type="text"
+                onChange={usernameChangeHandler}
+                required
+                placeholder="Enter Username"
+                className={styles.userDiv}
+              />
+              <input
+                type="password"
+                onChange={passwordChangeHandler}
+                required
+                placeholder="Enter Password"
+                className={styles.userDiv}
+              />
+              <button type="submit" className={styles.loginDiv}>
+                Login
+              </button>
             </form>
           </div>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default Login
+export default Login;
